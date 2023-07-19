98048b0a10a3d16bfc9202682db8752e1d293f03
muhammed rameez <31467986+mhmdrameez@users.noreply.github.com>
2023-07-19 14:34:23 +0530

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ const state = {
 }
 
 module.exports.connect=(done)=>{
-    const url = 'mongodb+srv://mhmdrameez:uJyZZz9dYFaEXd6o@cluster0.mkbor.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    const url = 'mongodb+srv://mhmdrameez:1@cluster0.mkbor.mongodb.net/?retryWrites=true&w=majority'
     const dbname = 'journal'
 
     mongoClient.connect(url,(err,data)=>{
